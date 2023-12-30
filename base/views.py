9a253008f180e5829e15c2ae4d935ebc3c060aca
dinukah99 <dinukahamarasinghe@gmail.com>
2023-12-31 01:18:18 +0530

    Initial Commit

---
+++ base/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('Home Page')
+
+
+def room(request):
+    return HttpResponse('Room Page')
