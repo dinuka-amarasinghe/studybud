71177e8e8a4b4f5e75df621dd5a72507575a66e7
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-02 16:52:54 +0530

    setting up DB and migrations

--- base/views.py
+++ base/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render
+from .models import Room
+
 
 # Create your views here.
 
-rooms = [
-    {
-        'id': 1,
-        'name': 'Lets learn Python',
-    },
-    {
-        'id': 2,
-        'name': 'Design with me',
-    },
-    {
-        'id': 3,
-        'name': 'Frontend developers',
-    }
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn Python'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+
+def create_room(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
