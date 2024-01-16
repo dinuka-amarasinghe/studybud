e84b69c039cbd22dd45aa70933880be3e6c46f58
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-16 13:26:56 +0530

    Recent activity component in home and user profiles

--- base/urls.py
+++ base/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('register/', views.registerPage, name='register'),
 
     path('', views.home, name='home'),
-    path('room/<str:pk>', views.room, name='room'),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
     path('create-room/', views.create_room, name='create-room'),
-    path('update-room/<str:pk>', views.update_room, name='update-room'),
-    path('delete-room/<str:pk>', views.delete_room, name='delete-room'),
-    path('delete-message/<str:pk>', views.delete_message, name='delete-message')
+    path('update-room/<str:pk>/', views.update_room, name='update-room'),
+    path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
+    path('delete-message/<str:pk>/', views.delete_message, name='delete-message')
 ]
