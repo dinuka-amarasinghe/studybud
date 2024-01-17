8841e2040fd8449291d3516493e6cefbc1b96d2e
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-17 23:43:35 +0530

    Update user profile settings functionality

--- base/urls.py
+++ base/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>/', views.update_room, name='update-room'),
     path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
-    path('delete-message/<str:pk>/', views.delete_message, name='delete-message')
+    path('delete-message/<str:pk>/', views.delete_message, name='delete-message'),
+    path('update-user/', views.update_user, name='update-user')
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -174,3 +174,16 @@ def delete_message(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': message})
+
+
+@login_required(login_url='login')
+def update_user(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update_user.html', {'form': form})
