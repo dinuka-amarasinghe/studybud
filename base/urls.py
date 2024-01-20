92d04181d48c6865d2f8e4341434dd3a4d1ad1ac
dinukah99 <dinukahamarasinghe@gmail.com>
2024-01-20 17:54:03 +0530

    Mobile responsive topics and activity components

--- base/urls.py
+++ base/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('update-room/<str:pk>/', views.update_room, name='update-room'),
     path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
     path('delete-message/<str:pk>/', views.delete_message, name='delete-message'),
-    path('update-user/', views.update_user, name='update-user')
+    path('update-user/', views.update_user, name='update-user'),
+    path('topics/', views.topics_page, name='topics'),
+    path('activity/', views.activity_page, name='activity')
 ]
--- base/views.py
+++ base/views.py
@@ -72,7 +72,7 @@ def home(request):
         Q(name__icontains=q) |
         Q(description__icontains=q)
     )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -187,3 +187,14 @@ def update_user(request):
             form.save()
             return redirect('user-profile', pk=user.id)
     return render(request, 'base/update_user.html', {'form': form})
+
+
+def topics_page(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+
+def activity_page(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
